1f0da7f0eacab8b7f836a83570251174eb1ef583
Paweł Tymosiak <szozr2@gmail.com>
2021-05-27 21:15:45 +0200

    Database fix Fixed Heroku Postrgres URL format.

--- database.py
+++ database.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from os import environ
 
-DATABASE_URL = environ['DATABASE_URL']
+DATABASE_URL = environ['DATABASE_URL'].replace('postgres:', 'postgresql:')
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
